343b720905ce08717492e83a64aa6679b085fc18
APAC\URM9KOR <rakesh.kumar8@in.bosch.com>
2020-07-20 13:04:15 +0530

    First Commit for arrays basic

---
+++ dataStructure/src/main/java/com/DS/Arrays/Lab1.java
@@ -0,0 +1,12 @@
+package com.DS.Arrays;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6,7,8};
+		for(int i = 0; i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
